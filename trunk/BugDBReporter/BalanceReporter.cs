222b8fa787653c6b16f0d386c6acbd13f927c73c
denis.kruchkov <denis.kruchkov@86cf4fb6-f753-11de-8cdf-e97ad7d0f28a>
2010-01-10 20:16:13 +0000

    Reporter ant tests projects added

---
+++ trunk/BugDBReporter/BalanceReporter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BugDB.Reporter
+{
+  /// <summary>
+  /// Creates different reports.
+  /// </summary>
+  /// <remarks>
+  /// +) Bug balance report
+  /// 
+  /// Balance report shows balance between bugs added and 
+  /// removed from consideration. 
+  /// 
+  /// Let's group states by their meaning in process:
+  /// 
+  /// ForAnalysis : open, reopen                   
+  /// ForFixing   : to_be_assigned, assigned, active
+  /// Postponed   : analysed, suspend
+  /// ForTesting  : invalid, duplicate, works_for_me, fixed, 
+  ///               modelling, regression, test_cases
+  /// Closed      : verified, closed, deleted
+  /// 
+  /// Different strategies could be used for treating both 
+  /// added and removed states:
+  /// 
+  /// 1) Added   : (ForTesting, Closed)->(ForAnalysis, ForFixing, Postponed)
+  ///    Removed : (ForAnalysis, ForFixing, Postponed)->(ForTesting, Closed) 
+  /// 
+  /// 2) Added   : (Postponed, ForTesting, Closed)->(ForAnalysis, ForFixing)
+  ///    Removed : (ForAnalysis, ForFixing)->(Postponed, ForTesting, Closed)
+  /// 
+  /// First approach considers Postopned bugs as removed
+  /// from development process (though temporary). Naturally 
+  /// when it's changed from Postponed to ForAnaysis or ForFixing
+  /// it's treated as Added. 
+  /// As opposed to that second approach treats Postponed as
+  /// something still for work thus - Added.
+  /// 
+  /// open
+  /// invalid
+  /// duplicate
+  /// analysed
+  /// suspend
+  /// to_be_assigned
+  /// assigned
+  /// works_for_me
+  /// active
+  /// fixed
+  /// reopen
+  /// modelling
+  /// regression
+  /// test_cases
+  /// verified
+  /// closed
+  /// deleted
+  /// 
+  /// </remarks>
+  public class BalanceReporter
+  {
+    /// <summary>
+    /// Creates balance report.
+    /// </summary>
+    public void CreateReport()
+    {
+      
+    }
+  }
+}
---
+++ trunk/BugDBReporterTests/BalanceReporterTest.cs
@@ -0,0 +1,97 @@
+﻿using System.IO;
+
+using BugDB.Aggregator;
+using BugDB.DataAccessLayer;
+using BugDB.DataAccessLayer.BLToolkitProvider;
+using BugDB.Reporter;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace BugDBReporterTests
+{
+  /// <summary>
+  /// Test class for BalanceReporter.
+  /// </summary>
+  [TestClass]
+  [DeploymentItem(@"DatabaseScripts\BugDB3.sql", "DatabaseScripts")]
+  [DeploymentItem(@"ReferenceData\approachOne.txt", "ReferenceData")]
+  public class BalanceReporterTest
+  {
+    #region Private Fields
+    private IDataProvider m_provider;
+    #endregion Private Fields
+
+    #region Public Properties
+    /// <summary>
+    /// Gets or sets the test context which provides
+    /// information about and functionality for the current test run.
+    /// </summary>
+    public TestContext TestContext { get; set; }
+    #endregion Public Properties
+
+    #region Additional test attributes
+    // 
+    //You can use the following additional attributes as you write your tests:
+    //
+    //Use ClassInitialize to run code before running the first test in the class
+    //[ClassInitialize()]
+    //public static void MyClassInitialize(TestContext testContext)
+    //{
+    //}
+    //
+    //Use ClassCleanup to run code after all tests in a class have run
+    //[ClassCleanup()]
+    //public static void MyClassCleanup()
+    //{
+    //}
+    //
+    //Use TestInitialize to run code before running each test
+    //[TestInitialize()]
+    //public void MyTestInitialize()
+    //{
+    //}
+    //
+    //Use TestCleanup to run code after each test has run
+    //[TestCleanup()]
+    //public void MyTestCleanup()
+    //{
+    //}
+    //
+    #endregion
+
+    /// <summary>
+    /// Initialize database only once for all tests
+    /// until they only query data.
+    /// </summary>
+    [TestInitialize]
+    public void TestInitialize()
+    {
+      if( m_provider == null )
+      {
+        // Path to create script
+        string dbCreateScriptPath = Path.Combine(
+          this.TestContext.TestDeploymentDir, @"DatabaseScripts\BugDB3.sql");
+        // Create provider
+        m_provider = new BLToolkitDataProvider(dbCreateScriptPath);
+        // Initialize storage (recreate database)
+        m_provider.InitializeStorage();
+        // Fill storage
+        StorageAggregator aggregator = new StorageAggregator(m_provider);
+        aggregator.FillStorage(Path.Combine(this.TestContext.TestDeploymentDir,
+                                            @"ReferenceData\approachOne.txt"));
+      }
+    }
+
+    /// <summary>
+    ///A test for CreateReport
+    ///</summary>
+    [TestMethod]
+    public void CreateReportTest()
+    {
+      var reporter = new BalanceReporter(); // TODO: Initialize to an appropriate value
+      reporter.CreateReport();
+      Assert.Inconclusive("A method that does not return a value cannot be verified.");
+    }
+  }
+}
